c07e549a75d78047e9ca16bfe86d19210cdff0fb
Hrvoje <hrvojehemen@gmail.com>
2022-12-30 14:01:34 +0100

    servisworker

---
+++ public/sw.js
@@ -0,0 +1,53 @@
+const filesToCache = [
+    "/",
+    "manifest.json",
+    "index.html",
+    "offline.html",
+    "error.html"
+];
+const cacheName = 'MyFancyCacheName_v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
+
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker installing…');
+});
+
+
+self.addEventListener('fetch', (event) => {
+    console.log("TU SAM")
+    console.log(event.request.url)
+    // https://stackoverflow.com/questions/49157622/service-worker-typeerror-when-opening-chrome-extension
+    if (event.request.url.startsWith('chrome-extension') || event.request.url.includes('extension')
+        || event.request.url.includes('sentry') || !event.request.url.startsWith('http')) return;
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then((response) => {
+                    // console.log("response.status = " + response.status);
+                    if (response.status === 404) {
+                        return caches.match("error.html");
+                    }
+                    return caches.open(cacheName).then((cache) => {
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                });
+            })
+            .catch((error) => {
+                console.log("Error", event.request.url, error);
+                return caches.match("offline.html");
+            })
+    );
+
+});
