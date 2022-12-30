a00608503f0eeaaec43d8352f30313b32cb9aa30
Hrvoje <hrvojehemen@gmail.com>
2022-12-30 11:36:32 +0100

    retrty

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'))
-app.listen(process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
-module.exports = app
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
